b07c2ab638e51765a3d34e0962ade6f95cdc5e53
Aakash Vishwakarma <aakashvish22@gmail.com>
2023-03-24 15:34:11 +0530

    Done with firebase integration

--- accident/views.py
+++ accident/views.py
@@ -1,32 +1,29 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
-from .forms import Accidentform
-from .forms import EmergencyForm
-from .models import Accident
-from .models import Emergency
 from django.contrib import messages
+from datetime import datetime
 import pyrebase
-from time import sleep
-
-
 
 firebaseConfig = {
-  'apiKey': "AIzaSyCAha5DfT2LigmUMI0fpKJmkxTbgBjpHgU",
-  'authDomain': "accident-b00b3.firebaseapp.com",
-  'databaseURL': "https://accident-b00b3-default-rtdb.asia-southeast1.firebasedatabase.app/",
-  'projectId': "accident-b00b3",
-  'storageBucket': "accident-b00b3.appspot.com",
-  'messagingSenderId': "31977108867",
-  'appId': "1:31977108867:web:788ea2a4548b71d6f2b2b6",
-  'measurementId': "G-3X8XBRMG6M"
+    "apiKey": "AIzaSyCoxQzhVKNb4QM1Rj5hDaWIQgMNji5jNY4",
+    "authDomain": "fir-login-crud-image.firebaseapp.com",
+    "projectId": "fir-login-crud-image",
+    "storageBucket": "fir-login-crud-image.appspot.com",
+    "messagingSenderId": "878884256845",
+    "appId": "1:878884256845:web:4107ffaba688fad5a9b004",
+    "databaseURL" : "https://fir-login-crud-image-default-rtdb.firebaseio.com/"
 }
 firebase = pyrebase.initialize_app(firebaseConfig)
 db = firebase.database()
 
+# Authentication
+auth = firebase.auth()
 
+# Storage. Stored like drive.
+storage = firebase.storage()
 
 def home(request):
     return render(request,"accident/home.html")
@@ -64,105 +61,190 @@ def logoutuser(request):
         return redirect('home')
 
 def userdetail(request):
-    if request.user.is_superuser == True:
-        return render(request, 'accident/admin.html')
-    else:
-        if request.method == 'GET':
-            return render(request, 'accident/userdetail.html', {'form': Accidentform})
-        else: 
-            accidents=Accident.objects.filter(user=request.user)
-            if(accidents):
-                msg = "You've already filled the form."
-                return render(request, 'accident/viewdetails.html', {'msg': msg})
-            else:
-                form = Accidentform(request.POST, request.FILES)
-                data = request.POST
-                db.child(data['user_name']).set(data)
-                new = form.save(commit=False)
-                new.user = request.user
-                new.save()
-                return redirect('viewdetails')
+  
+    if request.method == 'GET':
+        return render(request, 'accident/userdetail.html')
+    else: 
+        user = db.child("users").order_by_child("username").equal_to(request.user.username).limit_to_first(1).get()
+        if user.val():
+            return render(request, 'accident/viewdetails.html', {'msg': "You've already filled the form."})
+        else:
+            file= request.FILES['img']
+            file_format = file.name.split('.')[-1] 
+            fireb_upload = storage.child(f"users_image/{request.user}.{file_format}").put(file)
+            image_url=storage.child(f"users_image/{request.user}.{file_format}").get_url(None)
+
+            data = {
+                "username":request.user.username,
+                "full_name":request.POST['full-name'],
+                "email":request.POST['email'],
+                "mobile_no":request.POST['mobile-no'],
+                "vehicle_number_plate":request.POST['vehicle-number-plate'],
+                "address":request.POST['address'],
+                "bloodgrp": request.POST['blood-group'],
+                "emergency_no1":request.POST['emergency-no-1'],
+                "emergency_no2":request.POST['emergency-no-2'],
+                "user_image_url":image_url,
+                "user_image_token": fireb_upload['downloadTokens']
+            }
+            
+            # Saving with custom id: username
+            db.child("users").child(request.user).set(data)
+
+            return redirect('viewdetails')
             
 def viewdetails(request):
     if request.user.is_superuser == True:
-        accidents=Accident.objects.all()
-        # db.child("users").get()
-        return render(request, 'accident/adminuser.html', {'accidents': accidents})
+        users_list=[]
+        users = db.child("users").order_by_child("username").get()
+        if users.val():
+            for user in users.each():
+                users_list.append(user.val())
+        # users_list is stored in this format now: [{'address': 'New address', 'bloodgrp': 'AB+', 'email': 'diksha@gmail.com',...},{...}]
+
+        return render(request, 'accident/adminuser.html', {'users_list': users_list})
     else:
-        accidents = Accident.objects.filter(user=request.user)
-        # accident = accidents[0]
-        # # print(accident)
-        # # accident = dict(accident)
-        # accidents = db.child(accident).get().val()
-        # print(accident)
-        return render(request, 'accident/viewdetails.html', {'accidents': accidents})
+        user_details = db.child("users").order_by_child("username").equal_to(request.user.username).limit_to_first(1).get()
+        if user_details.val():
+            user_details=user_details.val()
+            for k, v in user_details.items():
+                user_details=v
+        
+        # user_details is stored in this format now: {'address': 'New address', 'bloodgrp': 'AB+', 'email': 'diksha@gmail.com',...}
+            return render(request, 'accident/viewdetails.html', {'user_details': user_details})
+        else:
+            return render(request, 'accident/viewdetails.html')
 
 def deletedetails(request):
-    accident = get_object_or_404(Accident, user=request.user)
     if request.method == 'POST':
-        accident.delete()
-        msgs = "Your details have been successfully deleted!"
-        return render(request, 'accident/viewdetails.html', {'msgs': msgs})
+        try:
+            user_details = db.child("users").order_by_child("username").equal_to(request.user.username).limit_to_first(1).get()
+            if user_details.val():
+                user_details=user_details.val()
+                for k, v in user_details.items():
+                    user_details=v
+            file_format = user_details['user_image_url'].split(".")[-1].split("?")[0]
+            # Deleting data from storage database
+            storage.delete(f"users_image/{request.user}.{file_format}",user_details['user_image_token'])
+            # Deleting data from realtime database
+            db.child("users").child(request.user.username).remove()
+            return render(request, 'accident/viewdetails.html', {'msgs': "Your details have been successfully deleted!"})
+        
+        except:
+            return render(request, 'accident/viewdetails.html', {'msgs': "Some server error occurred while deleting the details!"})
 
 def editdetails(request):
-    accident = get_object_or_404(Accident, user=request.user)
     if request.method == 'GET':
-        context = {'form': Accidentform(instance=accident)}
-        return render(request,'accident/editdetails.html',context)
-    elif request.method == 'POST':
-        form = Accidentform(request.POST,files=request.FILES, instance=accident)
-        if form.is_valid():
-            # form = Accidentform(request.POST, request.FILES)
-            data = request.POST
-            db.child(data['user_name']).set(data)
-            form.save()
-            return redirect('viewdetails')
-        else:
-            return render(request,'accident/editdetails.html',{'form':form})
+        user_details = db.child("users").order_by_child("username").equal_to(request.user.username).limit_to_first(1).get()
+        user_details=user_details.val()
+        for k, v in user_details.items():
+            user_details=v
+        return render(request,'accident/editdetails.html',{'user_details': user_details})
+    
+    elif request.method == 'POST':        
+
+        file= request.FILES['img']
+        file_format = file.name.split('.')[-1] 
+        # Replacement won't be done if extensions of image are different so:
+        # Deleting previous image file:
+        user_details = db.child("users").order_by_child("username").equal_to(request.user.username).limit_to_first(1).get()
+        if user_details.val():
+            user_details=user_details.val()
+            for k, v in user_details.items():
+                user_details=v
+        prev_file_format = user_details['user_image_url'].split(".")[-1].split("?")[0]
+        storage.delete(f"users_image/{request.user}.{prev_file_format}",user_details['user_image_token'])
+        
+        # Uploading new image file
+        fireb_upload = storage.child(f"users_image/{request.user}.{file_format}").put(file)
+        image_url=storage.child(f"users_image/{request.user}.{file_format}").get_url(None)
+        
+        data = {
+            "username":request.user.username,
+            "full_name": request.POST['full-name'],
+            "email":request.POST['email'],
+            "mobile_no":request.POST['mobile-no'],
+            "vehicle_number_plate":request.POST['vehicle-number-plate'],
+            "address":request.POST['address'],
+            "bloodgrp": request.POST['blood-group'],
+            "emergency_no1":request.POST['emergency-no-1'],
+            "emergency_no2":request.POST['emergency-no-2'],
+            "user_image_url":image_url,
+            "user_image_token": fireb_upload['downloadTokens']
+        }
+        
+        # Saving with custom id: username
+        db.child("users").child(request.user).set(data)
+        return redirect('viewdetails')
 
 def search_users(request):
     if request.method == "POST":
+        users_list=[]
         searched =request.POST['searched']
-        users = Accident.objects.filter(vehicle_number_plate__contains= searched)
+        # 'Contains' search query
+        users = db.child("users").order_by_child("vehicle_number_plate").end_at(searched+"\uf8ff").get()
+        for user in users.each():
+            users_list.append(user.val())
 
-        return render(request, 'accident/search_users.html',{'searched':searched, 'users':users})
+        return render(request, 'accident/search_users.html',{'searched':searched, 'users_list':users_list})
     else:
-        return render(request, 'accident/search_users.html',{})
+        return render(request, 'accident/search_users.html')
 
-def search_emergency(request):
-    if request.method == "POST":
-        searched =request.POST['searched']
-        users = Emergency.objects.filter(accident_vehicle_number__contains= searched)
-
-        return render(request, 'accident/search_emergency.html',{'searched':searched, 'users':users})
-    else:
-        return render(request, 'accident/search_emergency.html',{})
+# Emergency
 
 def emergencydetail(request):
-    if request.user.is_superuser == True:
-        return redirect('viewemergency')
+   
+    if request.method == 'GET':
+        return render(request, 'accident/emergencydetail.html')
     else:
-        if request.method == 'GET':
-            return render(request, 'accident/emergencydetail.html', {'form': EmergencyForm})
-        else:
-            try:
-                form = EmergencyForm(request.POST, request.FILES)
-                new = form.save(commit=False)
-                new.user = request.user
-                new.save()
+        try:
+            form_filled_date_time = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+            file= request.FILES['img']
+            file_format = file.name.split('.')[-1] 
+            storage.child(f"accidents_image/{request.POST['accident-vehicle-number']+form_filled_date_time}.{file_format}").put(file)
+            image_url=storage.child(f"accidents_image/{request.POST['accident-vehicle-number']+form_filled_date_time}.{file_format}").get_url(None)
 
-                msg = "You've successfully filled the form."
-                return render(request, 'accident/emergencydetail.html', {'form':EmergencyForm, 'msg':msg})
-            except:
-                return render(request, 'accident/emergencydetail.html', {'form':EmergencyForm, 'error':'Bad data passed in. Try again.'})
+            data = {
+                "full_name":request.POST['full-name'],
+                "mobile_no":request.POST['mobile-no'],
+                "accident_vehicle_number":request.POST['accident-vehicle-number'],
+                "accident_image_url":image_url,
+                "location_link": request.POST['location-link'],
+                "form_fill_date_time": form_filled_date_time
+            }
+            
+            # Saving with custom id: username
+            db.child("form_fillers").push(data)
+
+            return render(request, 'accident/emergencydetail.html', {'msg':"You've successfully filled the form."})
+        except:
+            return render(request, 'accident/emergencydetail.html', {'msg':"The form couldn't be saved."})
 
 def viewemergency(request):
     if request.user.is_superuser == True:
-        accidents= Emergency.objects.all()
-        return render(request, 'accident/admin.html', {'accidents': accidents})
+        accidents_list=[]
+        accidents = db.child("form_fillers").order_by_child("fill_date_time").get()
+        if accidents.val():
+            for accident in accidents.each():
+                accidents_list.append(accident.val())
+
+        return render(request, 'accident/admin.html', {'accidents_list': accidents_list})
+    # else:
+    #     accidents = Emergency.objects.all()
+    #     return render(request, 'accident/viewemergency.html', {'accidents': accidents})
+    
+def search_emergency(request):
+    if request.method == "POST":
+        accidents_list=[]
+        searched =request.POST['searched']
+        # 'Contains' search query
+        accidents = db.child("form_fillers").order_by_child("accident_vehicle_number").end_at(searched+"\uf8ff").get()
+        for accident in accidents.each():
+            accidents_list.append(accident.val())
+
+        return render(request, 'accident/search_emergency.html',{'searched':searched, 'accidents_list':accidents_list})
     else:
-        accidents = Emergency.objects.all()
-        return render(request, 'accident/viewemergency.html', {'accidents': accidents})
+        return render(request, 'accident/search_emergency.html')
 
 
 
--- accident_detection/urls.py
+++ accident_detection/urls.py
@@ -18,8 +18,7 @@ from django.urls import path
 from accident import views
 from django.conf.urls.static import static
 from django.conf import settings
-from django.conf.urls import url
-from django.views.static import serve
+
 
 
 urlpatterns = [
